fb51064b1c828d3958f6909a7e3f71a5a778a6b7
dtitov <dmytro.titov@gmail.com>
2019-11-22 14:41:31 +0000

    Deploying to gh-pages from master 3b24f117019f587c846f5fbaf1136f86f1405a1d

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"no.uio.ifi.tc"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"no.uio.ifi.tc"},{"l":"no.uio.ifi.tc.model"},{"l":"no.uio.ifi.tc.model.pojo"}]
