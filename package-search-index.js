ac8eba46991484d835f6e7b8e03f4130e6788d88
pavelvazquez <79310152+pavelvazquez@users.noreply.github.com>
2023-08-29 10:08:58 +0000

    Deploying to gh-pages from  @ c493a91d37740bfaad9fe1bf93102372c0826342 🚀

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"no.uio.ifi.tc"},{"l":"no.uio.ifi.tc.model"},{"l":"no.uio.ifi.tc.model.pojo"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"no.uio.ifi.tc"},{"l":"no.uio.ifi.tc.model"},{"l":"no.uio.ifi.tc.model.pojo"}];updateSearchResults();
